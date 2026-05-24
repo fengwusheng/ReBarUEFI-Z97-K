c83a6c621bd1ac5a06da7be8b7dd86568e41777a
fengwusheng <jimmyfengok@126.com>
2026-05-25 06:28:02 +0800

    Update ReBar.c

--- ReBarDxe/ReBar.c
+++ ReBarDxe/ReBar.c
@@ -328,7 +328,7 @@ EFI_STATUS EFIAPI rebarInit(
 	
 	DEBUG((DEBUG_INFO, "ReBarDXE: Boot Option Scanner Loaded.\n"));
 	
-    reBarState = 0;
+    reBarState = 9;
 	// 暴力遍历 Boot0000 到 Boot000F 这 16 个潜在的启动项
     /*for (bootIndex = 0; bootIndex <= 0x000F; bootIndex++) {
 		bootVarName[7] = (CHAR16)((bootIndex < 10) ? (L'0' + bootIndex) : (L'A' + (bootIndex - 10)));
